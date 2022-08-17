a94803868259794590ad0564c4e8f6deb4d61c70
lnjustin <lnjustin@users.noreply.github.com>
2022-08-17 16:29:27 -0400

    Update BPTWorld.QuickChartChild.groovy

--- BPTWorld.QuickChartChild.groovy
+++ BPTWorld.QuickChartChild.groovy
@@ -339,9 +339,20 @@ def getEventsHandler(evt) {
             if(logEnable) log.debug "In getEventsHandler (${state.version}) - Data File: ${fName}"
             readFile(fName)
             if(eventMap) {
-                if(dataType == "rawdata") {
-                    // don't have to do anymore processesing
-                    eventChartingHandler(eventMap)
+                if(dataType == "rawdata") { 
+                  //  if(logEnable) log.debug "In getEventsHandler - eventMap before sorting: ${eventMap}"
+                    def sortedMap = [:]
+                    if(reverseMap) {
+                        eventMap.each { key, value ->
+                            sortedMap[key] = value.sort { a, b -> Date.parse("yyyy-MM-dd HH:mm:ss.SSS", a.date) <=> Date.parse("yyyy-MM-dd HH:mm:ss.SSS", b.date) }
+                        }
+                     } else {
+                         eventMap.each { key, value ->
+                            sortedMap[key] = value.sort { a, b -> Date.parse("yyyy-MM-dd HH:mm:ss.SSS", b.date) <=> Date.parse("yyyy-MM-dd HH:mm:ss.SSS", a.date) }
+                        }
+                     }    
+                  //  if(logEnable) log.debug "In getEventsHandler - eventMap after sorting: ${sortedMap}"
+                    eventChartingHandler(sortedMap)
                 }
                 else if(theDays == "999") {
                     // don't have to do anymore processesing
