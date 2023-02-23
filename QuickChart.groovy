3940ce899a1d4d4582cca07c6b248fa1932bc525
lnjustin <lnjustin@users.noreply.github.com>
2023-02-23 06:48:01 -0500

    Create QuickChart.groovy

---
+++ QuickChart.groovy
@@ -0,0 +1,108 @@
+/**
+ *  **************** Quick Chart Parent ****************
+ *
+ *  Design Usage:
+ *  Chart your data, quickly and easily. Display your charts in any dashboard.
+ *
+ *  Copyright 2022 Bryan Turcotte (@bptworld)
+ *
+ *  This App is free.  If you like and use this app, please be sure to mention it on the Hubitat forums!  Thanks.
+ *
+ *  Remember...I am not a professional programmer, everything I do takes a lot of time and research!
+ *  Donations are never necessary but always appreciated.  Donations to support development efforts are accepted via: 
+ *
+ *  Paypal at: https://paypal.me/bptworld
+ *
+ *-------------------------------------------------------------------------------------------------------------------
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ * ------------------------------------------------------------------------------------------------------------------------------
+ *
+ *  If modifying this project, please keep the above header intact and add your comments/credits below - Thank you! -  @BPTWorld
+ *
+ *  App and Driver updates can be found at https://github.com/bptworld/Hubitat
+ *
+ * ------------------------------------------------------------------------------------------------------------------------------
+ *
+ *  Changes:
+ *
+ *  All changes are listed in the child app
+ */
+
+#include BPTWorld.bpt-normalStuff
+
+def setVersion(){
+    state.name = null
+}
+
+definition(
+    name:"Quick Chart",
+    namespace: "BPTWorld",
+    author: "Bryan Turcotte",
+    description: "Chart your data, quickly and easily. Display your charts in any dashboard.",
+    category: "Convenience",
+    iconUrl: "",
+    iconX2Url: "",
+    iconX3Url: ""
+)
+
+preferences {
+     page name: "mainPage", title: "", install: true, uninstall: true
+} 
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+    initialize()
+}
+
+def updated() {
+    log.debug "Updated with settings: ${settings}"
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    log.info "There are ${childApps.size()} child apps"
+    childApps.each {child ->
+    	log.info "Child app: ${child.label}"
+    }
+}
+
+def mainPage() {
+    dynamicPage(name: "mainPage") {
+    	installCheck()
+		if(state.appInstalled == 'COMPLETE'){
+			section("Instructions:", hideable: true, hidden: true) {
+				paragraph "<b>Information</b>"
+				paragraph "Chart your data, quickly and easily. Display your charts in any dashboard."
+			}
+			section(getFormat("header-green", "${getImage("Blank")}"+" Child Apps")) {
+				app(name: "anyOpenApp", appName: "Quick Chart Child", namespace: "BPTWorld", title: "<b>Add a new 'Quick Chart' child</b>", multiple: true)
+                app(name: "anyOpenApp", appName: "Quick Chart Data Collector Child", namespace: "BPTWorld", title: "<b>Add a new 'Quick Chart Data Collector ' child</b>", multiple: true)
+			}
+            
+			section(getFormat("header-green", "${getImage("Blank")}"+" General")) {
+       			label title: "Enter a name for parent app (optional)", required: false
+ 			}
+			display2()
+		}
+	}
+}
+
+def installCheck(){
+    display()
+	state.appInstalled = app.getInstallationState() 
+	if(state.appInstalled != 'COMPLETE'){
+		section{paragraph "Please hit 'Done' to install '${app.label}' parent app "}
+  	}
+  	else{
+    	log.info "Parent Installed OK"
+  	}
+}
